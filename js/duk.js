fcaadb8b8fdf5bc9acac0e4869ca6f9bfd12b708
Krazypants207 <killian@onyxwear.com>
2018-01-20 19:34:26 -0800

    Add YouTube API call logic

--- js/duk.js
+++ js/duk.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
   $('.gallery_photo').on('click', function (event) {
-    console.log(this)
     var image = $(this).attr('src')
-    console.log(image)
+
     $('#modal-img').attr('src', image)
+  })
+
+  $('.dropdown-toggle').on('click', function (e) {
+
+    var id = $(this).attr('id')
+    var route = 'https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=' + id + '&key=AIzaSyDiWtUOMzwnO32DZBa2y4Sozq9SPPWCoFs'
+    var request = $.ajax({
+      url: route,
+      method: 'GET'
+    })
+    $('.description').empty()
+    $('.channel-name').empty()
+    request.done(function (response) {
+      $('.description').append(response.items[0].snippet.description)
+      $('.channel-name').append(response.items[0].snippet.title)
+    })
   })
 })
